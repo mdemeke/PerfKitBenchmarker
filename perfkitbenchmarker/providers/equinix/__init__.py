70019b4ac657cef86c9e5f28ae697d32e4fbba4d
mdemeke <85367708+mdemeke@users.noreply.github.com>
2021-09-08 20:04:47 -0500

    UPDATED

--- perfkitbenchmarker/providers/equinix/__init__.py
+++ perfkitbenchmarker/providers/equinix/__init__.py
@@ -11,4 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""DigitalOcean cloud provider implementation."""
+"""Equinix Metal cloud provider implementation."""
